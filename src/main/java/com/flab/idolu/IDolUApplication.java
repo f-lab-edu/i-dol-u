86484af4c3c5faa02989b18d985cab081cac2b49
oneny <gjwodnjs1996@gmail.com>
2023-10-22 17:40:15 +0900

    initial project

---
+++ src/main/java/com/flab/idolu/IDolUApplication.java
@@ -0,0 +1,12 @@
+package com.flab.idolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDolUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDolUApplication.class, args);
+	}
+}
