160a4dd7d3e2358eedca3fd642a3c256a3324d8a
oneny <gjwodnjs1996@gmail.com>
2023-10-22 20:12:25 +0900

    Feat: Member 엔티티 생성

---
+++ src/main/java/com/flab/idolu/domain/member/entity/Member.java
@@ -0,0 +1,27 @@
+package com.flab.idolu.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Member {
+
+	private Long id;
+	private String email;
+	private String password;
+	private String name;
+	private String phone;
+	private RoleType roleType;
+	private boolean isDeleted;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+}
