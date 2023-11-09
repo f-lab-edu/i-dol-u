c04a63c31befa41d278735b3a66cc68092d79463
oneny <gjwodnjs1996@gmail.com>
2023-11-09 21:47:08 +0900

    Feat: 배송주소 등록 기능 controller 추가

---
+++ src/main/java/com/flab/idolu/domain/address/controller/AddressController.java
@@ -0,0 +1,41 @@
+package com.flab.idolu.domain.address.controller;
+
+import static com.flab.idolu.global.common.ResponseMessage.Status.*;
+import static com.flab.idolu.global.common.ResponseMessage.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.idolu.domain.address.dto.request.RegisterAddressDto;
+import com.flab.idolu.domain.address.service.AddressService;
+import com.flab.idolu.global.annotation.MemberLoginCheck;
+import com.flab.idolu.global.common.ResponseMessage;
+import com.flab.idolu.global.util.SessionUtil;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/addresses")
+@RequiredArgsConstructor
+public class AddressController {
+
+	private final AddressService addressService;
+
+	@PostMapping
+	@MemberLoginCheck
+	public ResponseEntity<ResponseMessage> registerAddress(
+		@RequestBody RegisterAddressDto registerAddressDto,
+		HttpSession httpSession) {
+
+		Long memberId = SessionUtil.getLoginMemberId(httpSession);
+		addressService.registerAddress(registerAddressDto, memberId);
+
+		return ResponseEntity.ok(builder()
+			.status(SUCCESS)
+			.build());
+	}
+}
