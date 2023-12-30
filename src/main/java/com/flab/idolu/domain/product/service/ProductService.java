d9d19349b37c360101c2f415b202bf8f240c8e52
oneny <gjwodnjs1996@gmail.com>
2023-12-30 21:39:27 +0900

    Refactor: 읽기 전용 Transactional 추가

--- src/main/java/com/flab/idolu/domain/product/service/ProductService.java
+++ src/main/java/com/flab/idolu/domain/product/service/ProductService.java
@@ -3,6 +3,7 @@ package com.flab.idolu.domain.product.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.flab.idolu.domain.product.dto.response.ProductListResponseDto;
 import com.flab.idolu.domain.product.dto.response.ProductPaginationDto;
@@ -16,6 +17,7 @@ public class ProductService {
 
 	private final ProductRepository productRepository;
 
+	@Transactional(readOnly = true)
 	public ProductListResponseDto findByCategoryIdAndIDolId(Long categoryId, Long iDolId,
 		int offset, int size, String order) {
 
