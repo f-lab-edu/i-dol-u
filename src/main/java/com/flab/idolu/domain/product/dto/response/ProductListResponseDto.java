bd770daa16ea79d0825d959faa7523b190a92bb3
oneny <gjwodnjs1996@gmail.com>
2023-12-30 18:36:10 +0900

    Feat: ProductListResponseDto 생성

---
+++ src/main/java/com/flab/idolu/domain/product/dto/response/ProductListResponseDto.java
@@ -0,0 +1,19 @@
+package com.flab.idolu.domain.product.dto.response;
+
+import java.math.BigDecimal;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProductListResponseDto {
+
+	private Long id;
+	private Long categoryId;
+	private Long iDolId;
+	private String name;
+	private BigDecimal price;
+	private Integer stock;
+	private String imageUrl;
+}
