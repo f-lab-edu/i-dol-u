9dab6f6acb64dbfb710a72029b60970584d7a363
oneny <gjwodnjs1996@gmail.com>
2023-12-31 14:52:03 +0900

    Feat: Order 엔티티 수정

--- src/main/java/com/flab/idolu/domain/order/entity/Order.java
+++ src/main/java/com/flab/idolu/domain/order/entity/Order.java
@@ -3,6 +3,17 @@ package com.flab.idolu.domain.order.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Order {
 
 	private Long id;
