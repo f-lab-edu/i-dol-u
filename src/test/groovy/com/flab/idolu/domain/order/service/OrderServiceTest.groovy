c85c9ba50f5007f7258bf6a7de852b645a2e0d3c
oneny <gjwodnjs1996@gmail.com>
2024-01-01 11:26:25 +0900

    Test: 결제 기능 유효성 검증 테스트 추가

--- src/test/groovy/com/flab/idolu/domain/order/service/OrderServiceTest.groovy
+++ src/test/groovy/com/flab/idolu/domain/order/service/OrderServiceTest.groovy
@@ -1,9 +1,8 @@
 package com.flab.idolu.domain.order.service
 
-
 import com.flab.idolu.domain.order.repository.OrderRepository
-import com.flab.idolu.domain.product.exception.InsufficientStockException
-import com.flab.idolu.domain.product.exception.ProductNotFoundException
+import com.flab.idolu.domain.payment.repository.PaymentRepository
+import com.flab.idolu.domain.payment.service.PaymentService
 import com.flab.idolu.domain.product.repository.ProductRepository
 import com.flab.idolu.domain.product.service.ProductService
 import spock.lang.Specification
@@ -16,12 +15,15 @@ class OrderServiceTest extends Specification {
 
     OrderService orderService;
     ProductService productService;
+    PaymentService paymentService;
     OrderRepository orderRepository = Mock()
     ProductRepository productRepository = Mock()
+    PaymentRepository paymentRepository = Mock()
 
     def setup() {
         productService = new ProductService(productRepository)
-        orderService = new OrderService(orderRepository, productService)
+        paymentService = new PaymentService(paymentRepository)
+        orderService = new OrderService(orderRepository, productService, paymentService)
     }
 
     def "주문 유효성 검증 실패 테스트: #exceptionMessage"() {
@@ -33,37 +35,14 @@ class OrderServiceTest extends Specification {
         exception.message == exceptionMessage
 
         where:
-        orderRequest                  | exceptionMessage
-        BLANK_RECIPIENT_ORDER_REQUEST | "수령자를 입력해야 합니다."
-        BLANK_PHONE_ORDER_REQUEST     | "휴대전화를 입력해야 합니다."
-        BLANK_ZIPCODE_ORDER_REQUEST   | "우편번호를 입력해야 합니다."
-        BLANK_ADDRESS1_ORDER_REQUEST  | "기본주소를 입력해야 합니다."
-        INVALID_PHONE_ORDER_REQUEST   | "휴대전화 양식에 맞춰야 합니다."
-        EMPTY_ITEMS_ORDER_REQUEST     | "구매 상품이 없습니다."
-    }
-
-    def "주문 실패 테스트: 재고 없음"() {
-        given:
-        productRepository.findById(1L) >> Optional.empty()
-
-        when:
-        orderService.placeOrder(DEFAULT_ORDER_REQUEST, 1L)
-
-        then:
-        def exception = thrown(ProductNotFoundException)
-        exception.message == "상품이 없습니다."
-    }
-
-    def "주문 실패 테스트: 재고 부족"() {
-        given:
-        productRepository.findById(1L) >> of(DEFAULT_PRODUCT_FOR_ORDER)
-
-        when:
-        orderService.placeOrder(INSUFFICIENT_ORDER_REQUEST, 1L)
-
-        then:
-        def exception = thrown(InsufficientStockException)
-        exception.message == "재고는 0개 미만이 될 수 없습니다."
+        orderRequest                       | exceptionMessage
+        BLANK_RECIPIENT_ORDER_REQUEST      | "수령자를 입력해야 합니다."
+        BLANK_PHONE_ORDER_REQUEST          | "휴대전화를 입력해야 합니다."
+        BLANK_ZIPCODE_ORDER_REQUEST        | "우편번호를 입력해야 합니다."
+        BLANK_ADDRESS1_ORDER_REQUEST       | "기본주소를 입력해야 합니다."
+        INVALID_PHONE_ORDER_REQUEST        | "휴대전화 양식에 맞춰야 합니다."
+        EMPTY_ITEMS_ORDER_REQUEST          | "구매 상품이 없습니다."
+        INVALID_PAYMENT_TYPE_ORDER_REQUEST | "PAY에 해당하는 결제 타입이 없습니다."
     }
 
     def "주문 성공 테스트"() {
