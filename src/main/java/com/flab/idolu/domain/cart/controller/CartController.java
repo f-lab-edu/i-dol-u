582258d39a7d691aa6140878b33010567ee1e1d4
oneny <gjwodnjs1996@gmail.com>
2024-01-28 16:15:11 +0900

    Feat: 카트 상품 담기 기능 controller 추가

---
+++ src/main/java/com/flab/idolu/domain/cart/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.flab.idolu.domain.cart.controller;
+
+import static com.flab.idolu.global.common.ResponseMessage.Status.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.idolu.domain.cart.dto.request.CartProductRequest;
+import com.flab.idolu.domain.cart.service.CartService;
+import com.flab.idolu.global.annotation.MemberLoginCheck;
+import com.flab.idolu.global.annotation.SessionMemberId;
+import com.flab.idolu.global.common.ResponseMessage;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carts")
+public class CartController {
+
+	private final CartService cartService;
+
+	@PostMapping
+	@MemberLoginCheck
+	public ResponseEntity<ResponseMessage> createCartProduct(
+		@RequestBody CartProductRequest cartProductRequest,
+		@SessionMemberId Long memberId) {
+		
+		cartService.insertCart(cartProductRequest, memberId);
+
+		return ResponseEntity.ok(ResponseMessage.builder()
+			.status(SUCCESS)
+			.build());
+	}
+}
