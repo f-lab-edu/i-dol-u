1e263adc7e3c556e4b863d6d0d017683e1534a60
oneny <gjwodnjs1996@gmail.com>
2024-02-01 13:02:19 +0900

    Feat: 카트 상품 조회 mapper 구현

--- src/main/java/com/flab/idolu/domain/cart/repository/CartRepository.java
+++ src/main/java/com/flab/idolu/domain/cart/repository/CartRepository.java
@@ -1,11 +1,13 @@
 package com.flab.idolu.domain.cart.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.flab.idolu.domain.cart.dto.response.CartProductResponse;
 import com.flab.idolu.domain.cart.entity.Cart;
 
 @Mapper
@@ -17,4 +19,6 @@ public interface CartRepository {
 	Optional<Cart> findByProductIdAndMemberId(@Param("productId") Long productId, @Param("memberId") Long memberId);
 
 	void updateCartQuantity(Cart cart);
+
+	List<CartProductResponse> findByMemberId(Long memberId);
 }
