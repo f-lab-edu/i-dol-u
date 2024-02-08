d96484d5b408e3fa6f3aa0a023fab4e012bbdf76
oneny <gjwodnjs1996@gmail.com>
2024-02-08 20:40:46 +0900

    Feat: 카트 삭제 api 추가

--- src/main/java/com/flab/idolu/domain/cart/controller/CartController.java
+++ src/main/java/com/flab/idolu/domain/cart/controller/CartController.java
@@ -4,12 +4,12 @@ import static com.flab.idolu.global.common.ResponseMessage.Status.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flab.idolu.domain.cart.dto.request.CartProductRequest;
@@ -33,7 +33,7 @@ public class CartController {
 	public ResponseEntity<ResponseMessage> createCartProduct(
 		@RequestBody CartProductRequest cartProductRequest,
 		@SessionMemberId Long memberId) {
-		
+
 		cartService.insertCart(cartProductRequest, memberId);
 
 		return ResponseEntity.ok(ResponseMessage.builder()
@@ -57,8 +57,21 @@ public class CartController {
 		@SessionMemberId Long memberId,
 		@PathVariable Long cartId,
 		@RequestBody ModifyCartQuantityRequest modifyCartQuantityRequest
-		) {
+	) {
 		cartService.updateCartQuantity(cartId, modifyCartQuantityRequest, memberId);
+
+		return ResponseEntity.ok(ResponseMessage.builder()
+			.status(SUCCESS)
+			.build());
+	}
+
+	@MemberLoginCheck
+	@PatchMapping("/{cartId}/delete")
+	public ResponseEntity<ResponseMessage> updateCartQuantity(
+		@SessionMemberId Long memberId,
+		@PathVariable Long cartId
+	) {
+		cartService.updateDeletedById(cartId, memberId);
 
 		return ResponseEntity.ok(ResponseMessage.builder()
 			.status(SUCCESS)
